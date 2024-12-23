8bd5f6a43b529969276f92fd3748992f2fa97726
ImPhatDat <phatdatn2003@gmail.com>
2024-12-23 11:20:03 +0000

    remove unnecessary main function

--- optimizations/version1/compile.sh
+++ optimizations/version1/compile.sh
@@ -1,4 +1,4 @@
-nvcc main.cu ../../src_parallel/utils/fashion_mnist.cu \
+nvcc ../../src_parallel/main.cu ../../src_parallel/utils/fashion_mnist.cu \
     ../../src_parallel/layer/layer.cu ../../src_parallel/layer/softmax.cu ../../src_parallel/layer/relu.cu layer/dense.cu \
     ../../src_parallel/loss/loss.cu ../../src_parallel/loss/categorical_crossentropy.cu \
     ../../src_parallel/Model/Model.cu \
