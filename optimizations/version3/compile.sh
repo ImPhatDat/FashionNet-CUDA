94e77311de9cb9291366aaae5c5154341dc8180c
Tran Nguyen Huan <93178699+knightstark7@users.noreply.github.com>
2024-12-23 20:05:31 +0700

    update version 3: matmul

---
+++ optimizations/version3/compile.sh
@@ -0,0 +1,6 @@
+nvcc main.cu src/utils/fashion_mnist.cu \
+    src/layer/layer.cu src/layer/softmax.cu src/layer/relu.cu layer/dense.cu \
+    src/loss/loss.cu src/loss/categorical_crossentropy.cu \
+    src/Model/Model.cu \
+    src/metrics/accuracy.cu \
+    -o a.out
